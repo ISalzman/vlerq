b80c976e50589e2a54749acf7cfa168ccb4b40d0
Jean-Claude Wippler <jc@wippler.nl>
2014-07-02 01:55:33 +0200

    add basic structures and methods

--- vlerq.go
+++ vlerq.go
@@ -1,1 +1,53 @@
 package vlerq
+
+type View struct {
+	rows uint32
+	keys uint8
+	uniq uint8
+	meta *View
+	data []Column
+}
+
+func NewView(mv View) *View {
+	v := new(View)
+	v.meta = &mv
+	v.data = make([]Column, mv.Cols())
+	return v
+}
+
+func (v *View) Rows() int {
+	return int(v.rows)
+}
+
+func (v *View) Cols() int {
+	return v.meta.Rows()
+}
+
+func (v *View) Meta() *View {
+	return v.meta
+}
+
+type Column interface{
+	At(i int) interface{}
+	Set(i int, v interface{}) interface{}
+}
+
+type IntColumn []int
+
+func (c IntColumn) At(i int) interface{} {
+	return c[i]
+}
+
+func (c IntColumn) Set(i int, v interface{}) {
+	c[i] = v.(int)
+}
+
+type StringColumn []string
+
+func (c StringColumn) At(i int) interface{} {
+	return c[i]
+}
+
+func (c StringColumn) Set(i int, v interface{}) {
+	c[i] = v.(string)
+}
--- vlerq_test.go
+++ vlerq_test.go
@@ -1,18 +1,18 @@
 package vlerq_test
 
 import (
-	// . "github.com/jcw/vlerq"
+	. "github.com/jcw/vlerq"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Flow", func() {
 
-	Describe("Create a Gadget", func() {
-		g := 1
+	Describe("Create a View", func() {
+		v := new(View)
 
 		It("should not be nil", func() {
-			Expect(g).ShouldNot(BeNil())
+			Expect(v).ShouldNot(BeNil())
 		})
 	})
 })
